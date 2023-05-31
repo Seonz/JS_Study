5bda8cbdfe9586b9bb8cf37f55e9333eddd5881a
Seonz <zxzx00202@naver.com>
2023-05-31 17:11:26 +0900

    자바스크립트 문장 이해

---
+++ part1/006-2.js
@@ -0,0 +1,7 @@
+function greeting() {
+    "hello"
+    "Chloe" +3
+    greeting()
+}
+// greeting(if(true) {console.log("It is not acceptable") })
+// 함수 인자에는 값을 넣어야하나, 값으로 의도된 곳에 표현식 대신 if 명령문을 넣음 구문오류
