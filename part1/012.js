a38f6e31b4015f80145ff771712117ecefa51511
Seonz <zxzx00202@naver.com>
2023-06-01 16:24:09 +0900

    반복문 for 활용

---
+++ part1/012.js
@@ -0,0 +1,15 @@
+var subject = '자바스크립트';
+switch (subject) {
+    case 'C언어':
+        console.log('c언어 입니다.');
+        break;
+    case '자바스크립트':
+        console.log('자바스크립트 입니다.');
+        break;
+    case '파이썬':
+        console.log('파이썬 입니다.');
+        break;
+    default:
+        console.log('아무것도 아닙니다.');
+        break;
+}
